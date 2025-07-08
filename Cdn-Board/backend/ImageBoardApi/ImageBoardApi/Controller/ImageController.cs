8fc7e833d217a21e93e56c22ad028a9114bd0a36
Mahesh bisht <mahesh.bisht3112@outlook.com>
2025-07-08 23:07:00 +0530

    checkin backend code

---
+++ Cdn-Board/backend/ImageBoardApi/ImageBoardApi/Controller/ImageController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageBoardApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        [HttpGet("{filename}")]
+        public async Task<IActionResult> GetImage(string filename)
+        {
+            string currentDir = Directory.GetCurrentDirectory();
+            var path = Path.Combine(currentDir,"Images", filename);
+
+            if (!System.IO.File.Exists(path)) return NotFound();
+
+            var bytes = await System.IO.File.ReadAllBytesAsync(path);
+            return File(bytes, "image/jpeg", enableRangeProcessing: true);
+        }
+    }
+}
